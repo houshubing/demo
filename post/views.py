1d77a02e35ded0968bfd971777ff00f6904b28a1
侯书兵 <38975416+houshubing@users.noreply.github.com>
2018-08-03 10:20:19 +0800

    Feature zj (#5)
    * 添加了缓存redis
    
    * 用装饰器设置缓存
    
    * 完成了阅读排行模块
    
    * 添加了中间件防爬虫

--- post/views.py
+++ post/views.py
@@ -1,9 +1,15 @@
 from math import ceil
 
+
 from django.shortcuts import render, redirect
+
 from post.models import Post
+from post.helper import page_cache
+from post.helper import read_count
+from post.helper import get_top_n
 
 
+@page_cache(10)
 def post_list(request):
     page = int(request.GET.get('page', 1))  #页码
     per_page = 10                           #每页文章数
@@ -33,6 +39,7 @@ def edit_post(request):
         post.title = request.POST.get('title')
         post.content = request.POST.get('content')
         post.save()
+
         return redirect('/post/read/?post_id=%s' % post.id)
     else:
         post_id = int(request.GET.get('post_id'))
@@ -40,6 +47,8 @@ def edit_post(request):
         return render(request, 'edit_post.html', {'post': post})
 
 
+@read_count
+@page_cache(10)
 def read_post(request):
     post_id = int(request.GET.get('post_id'))
     post = Post.objects.get(id=post_id)
@@ -50,3 +59,13 @@ def search(request):
     keyword = request.POST.get('keyword')
     posts = Post.objects.filter(content__contains=keyword)
     return render(request, 'search.html', {'posts': posts})
+
+
+def top10(request):
+    '''
+    1. aaaa 30
+    2. bbbb 27
+    3. cccc 19
+    '''
+    rank_data = get_top_n(10)
+    return render(request, 'top10.html', {'rank_data': rank_data})
